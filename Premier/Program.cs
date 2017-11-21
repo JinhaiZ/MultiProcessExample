08b20d2113286e52d2c6976877a2fdab48d477df
JinhaiZ <zhoujinhaixd@gmail.com>
2017-11-21 13:55:55 +0100

    add premier.exe and add first menu functionality

---
+++ Premier/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Premier
+{
+    class NombrePremier
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // un programme simple du critère d'Eratosthène pour la 
+            // recherche de nombres premiers suoérieur à 1
+            // On ne s'arrête pas à la racine carrée
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.SetWindowSize(7, 20);  // la taille de la fenêtre
+
+
+            for (int p = 1; p < 1000000; p++)
+            {
+                int i = 2;
+                while ((p % i) != 0 && i < p)
+                {
+                    i++;
+                }
+                if (i == p)
+                    Console.WriteLine(p.ToString());
+                Thread.Sleep(50);
+
+            }
+        }
+
+
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
 
 namespace WpfApp1
 {
@@ -21,26 +22,93 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Process> listProcess = new List<Process>();
+        private LinkedList<ProcessNode> listProcess = new LinkedList<ProcessNode>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void startButton_Click(object sender, RoutedEventArgs e)
+        private void startBallon_Click(object sender, RoutedEventArgs e)
         {
-            if (listProcess.Count < 5)
+            if (listProcess.Count == 0 || listProcess.First().getBallonCount() < 5)
             {
-                ProcessStartInfo ballonProcess = new ProcessStartInfo("C:\\Users\\jinha\\Dropbox\\2017-2018 Télécom Bretagne F2B\\F2B-205-NET\\Ballon.exe");
+                ProcessStartInfo ballonProcess = new ProcessStartInfo("Ballon.exe");
+
                 Process p = Process.Start(ballonProcess);
-                listProcess.Add(p);
+                int pid = p.Id;
+                listProcess.AddLast(new ProcessNode(p, "ballon"));
+                lvProcess.Items.Add(new ProcessItem() { ProcessName = "ballon.exe", PID = pid });
+                ballonCountView.Text = listProcess.First().getBallonCount().ToString();
+                countView.Text = (listProcess.First().getBallonCount() + listProcess.First().getPremierCount()).ToString();
             }
             else
             {
                 MessageBox.Show("Cannot create more than 5 processes", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
             }
+        }
 
-            
+        private void startPremier_Click(object sender, RoutedEventArgs e)
+        {
+            if (listProcess.Count == 0 || listProcess.First().getPremierCount() < 5)
+            {
+                ProcessStartInfo premierProcess = new ProcessStartInfo("premier.exe");
+
+                Process p = Process.Start(premierProcess);
+                int pid = p.Id;
+                listProcess.AddLast(new ProcessNode(p, "premier"));
+                lvProcess.Items.Add(new ProcessItem() { ProcessName = "premier.exe", PID = pid });
+                premierCountView.Text = listProcess.First().getPremierCount().ToString();
+                countView.Text = (listProcess.First().getBallonCount() + listProcess.First().getPremierCount()).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Cannot create more than 5 processes", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private void lvProcess_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+
+    public class ProcessItem
+    {
+        public string ProcessName { get; set; }
+        public int PID { get; set; }
+    }
+
+    public class ProcessNode
+    {
+        public Process process;
+        public string name;
+        public static int ballonCount = 0;
+        public static int premierCount = 0;
+
+        public ProcessNode(Process process, string name)
+        {
+            this.process = process;
+            this.name = name;
+            if (name == "ballon")
+            {
+                ballonCount += 1;
+            }
+            else
+            {
+                premierCount += 1;
+            }
+        }
+
+        public int getBallonCount()
+        {
+            return ballonCount;
+        }
+        public int getPremierCount()
+        {
+            return premierCount;
         }
     }
+
 }
